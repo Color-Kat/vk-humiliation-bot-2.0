5bbf058a9f4a382ab1fe03d438cd2c0df521f458
Color-Kat <katik7979@gmail.com>
2021-10-29 20:49:58 +0300

    checking pattern by RegExp. It is very cool;)

--- app/humiliationBot/strategies/AbstractStrategy.php
+++ app/humiliationBot/strategies/AbstractStrategy.php
@@ -22,20 +22,42 @@ class AbstractStrategy extends VkMessage
 
     public function getMatch(string $message, array $dictionary)
     {
+        $match = null;
+
+        // iterate over all answers and search pattern match
         foreach ($dictionary['answers'] as $answer) {
 
             $pattern = $answer['pattern'];
 
-            $insults = ['дурак', 'дурашка', 'идиот'];
+            // ----- substitute values from wordbook ----- //
+            // search all (@var) and save it in $vars
+            if (preg_match_all('/\(@\w+\)/u', $pattern, $vars)) {
+                // iterate over $vars and substitute values from the wordbook
+                foreach ($vars[0] as $var) {
+                    // get var from (@var)
+                    $var = preg_replace('/[@\(\)]/', '', $var);
 
-            $pattern = preg_replace(
-                '/\(@insult\)/',
-                '(' . implode('|', $this->wordbook['insult']) . ')',
-                $pattern
-            );
+                    // and finally substitute values
+                    $pattern = preg_replace(
+                        "/\(@$var\)/u",
+                        '(' . implode('|', $this->wordbook[$var]) . ')',
+                        $pattern
+                    );
+                }
+            }
 
-            Log::info('pattern', $pattern);
+            // add /ui flags to support russian language and case insensitivity
+            $pattern .= 'ui';
+
+            // check match
+            if (preg_match($pattern, $message) && ($answer['priority'] ?? 0) >= ($match['priority'] ?? 0)) {
+                $match = $answer;
+            }
+
+            Log::info('pattern: ', $pattern);
         }
+
+        Log::info('match: ', $match['priority']);
 
         return 123;
     }
