f3685a058ad72a91841dbdc4a947731519c4adf0
Color-Kat <katik7979@gmail.com>
2022-03-23 17:24:06 +0300

    diminutive_name

--- app/humiliationBot/DictionaryFunctions.php
+++ app/humiliationBot/DictionaryFunctions.php
@@ -4,15 +4,34 @@ namespace humiliationBot;
 
 class DictionaryFunctions
 {
-    public function rand(array $elements) {
+    public function rand(array $elements)
+    {
         return $elements[array_rand($elements)];
     }
 
-    public function caps(string $mess) {
+    public function caps(string $mess)
+    {
         return mb_strtoupper($mess);
     }
 
-    public function repeat(array $elements): string {
+    public function repeat(array $elements): string
+    {
         return str_repeat($elements[0], $elements[1]);
+    }
+
+    public function diminutive_name(array $elements): string
+    {
+        $name = $elements[0];
+        $length = mb_strlen($name);
+        $diminutive_name = 'кляп' . mb_substr($name, $length - 3, $length);
+        // Саша - кляпаша
+        // ира - кляпира
+        // Александр - кляпндр
+        // Соня - кляпоня
+        // Иван - кляпван
+        // Дима - кляпима
+        // Егор - кляпор
+        // Дмитрий - кляприй
+        return $diminutive_name;
     }
 }
